e3fc45e91ff5b95f61bb8b83286902b2796afff8
YanHSana <1269987538@qq.com>
2021-09-04 14:35:50 +0800

    新建菜单、头部标题文件

---
+++ src/component/headtitle/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function headtitle(){
+  return(
+    <div className='head'>
+      head
+    </div>
+  )
+}
+export default headtitle;
---
+++ src/component/menu/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function menu(){
+  return(
+    <div className='menu'>
+
+    </div>
+  )
+}
+export default menu;
