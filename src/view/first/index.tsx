14b38293be87eceb2fda2ea5b4d638ea3e278a96
YanHSana <1269987538@qq.com>
2021-09-16 23:26:43 +0800

    函数组件使用方法，函数组件的多条件渲染-使用纯函数组件

--- src/view/first/index.tsx
+++ src/view/first/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import {withRouter} from 'react-router-dom'
 import { Button } from 'antd';
 import axios from 'axios'
@@ -10,18 +10,63 @@ axios.defaults.baseURL = 'http://localhost:9000';
 //     console.log(res);
 //   })
 // }
-function getDate(){
-  axios.get('/community').then(res => {
-    console.log(res);  
-  })
+// function getDate(){
+//   axios.get('/community').then(res => {
+//     console.log(res);  
+//   })
+// }
+function methostest1(a, b){
+  console.log(a + b);
+  return a+b;
 }
 
-function first(props:any){
+function Acom(props){
+  const [title, settitle] = useState(props.title)
+  useEffect(() => {
+    console.log(title);
+    console.log(props);
+  },[title])
   return(
-    <div className='first-com' key={props.location.key}>
-      first
-      {/* <Button type="primary" onClick={getDate}>Button-First</Button> */}
+    <div>
+      {title}
+      <Button onClick={() => settitle(title + 'e')}>Click</Button>
+    </div>
+  )
+}
+
+function NumTest(props){
+  return <><span>{props.numContent}</span></>
+}
+
+function NumNone(props){
+  return <><span>{props.numContent}</span></>
+}
+function NumChange(props){
+  const {numContent, numClor} = props.dataSource;
+  if(numContent == '1000'){
+    return <NumTest numContent={numContent} />
+  }else{
+    return <NumNone numContent={numContent} />
+  }
+}
+function first(){
+  let dataSource = {
+    numContent: '1000',
+    numColor: 'successColor'
+  }
+
+  let testA = methostest1(5,7);
+  console.log(testA);
+  
+  return(
+    <div className='first-com'>
+     <h1>First-Component</h1>
+     <p>{testA}</p>
+      <Acom title='helloReactsss...'></Acom>
+      <NumChange dataSource={dataSource}></NumChange>
     </div>
   )
 }
 export default withRouter(first);
+
+
