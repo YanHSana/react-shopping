94df6f668774b8c44e2e65ddb4d8bc846f2a9ffa
YanHSana <1269987538@qq.com>
2021-09-25 18:30:42 +0800

    首页请求数据展示，样式并未进行调试

--- src/view/category/index.tsx
+++ src/view/category/index.tsx
@@ -1,12 +1,52 @@
 
-import React from 'react';
+import React,{useState, useEffect} from 'react';
+import axios from 'axios';
+import Item from 'antd/lib/list/Item';
+axios.defaults.baseURL="https://api-hmugo-web.itheima.net/api/public/v1";
 
-function category(){
+
+function Category(){
+
+  const [category, setCategory] = useState([]);
+
+  const getCategory = () => {
+    axios.get('/categories').then(res => {
+      console.log(res);
+      setCategory(res?.data?.message);
+    })
+  }
+
+  useEffect(() => {
+    getCategory();
+  },[])
+
   return(
     <div className='category-com'>
-      category
+      <div className='left-category'>
+        {
+          category && category.map((item, index) => {
+            return(
+              <div key={index}>
+                <p>{item?.cat_name}</p>
+                {
+                  item?.children && item?.children.map((_item, _index) => {
+                    return(
+                      <div key={_index}>
+                        <p>--{_item?.cat_name}</p>
+                      </div>
+                    )
+                  })
+                }
+              </div>
+            )
+          })
+        }
+      </div>
+      <div className='detail-every-category'>
+
+      </div>
     </div>
   )
 }
 
-export default category;
+export default Category;
--- src/view/first/index.tsx
+++ src/view/first/index.tsx
@@ -1,72 +1,99 @@
 import React, {useState, useEffect} from 'react'
-import {withRouter} from 'react-router-dom'
-import { Button } from 'antd';
-import axios from 'axios'
-// 请求数据
-// axios.defaults.baseURL="https://api-hmugo-web.itheima.net/api/public/v1"
-axios.defaults.baseURL = 'http://localhost:9000';
-// function getDate():void{
-//   axios.get('/community').then((res) => {
-//     console.log(res);
-//   })
-// }
-// function getDate(){
-//   axios.get('/community').then(res => {
-//     console.log(res);  
-//   })
-// }
-function methostest1(a, b){
-  console.log(a + b);
-  return a+b;
-}
+// import {withRouter} from 'react-router-dom'
+import { Button, Carousel, Row, Col  } from 'antd';
+import axios from 'axios';
+import './index.css'
 
-function Acom(props){
-  const [title, settitle] = useState(props.title)
+axios.defaults.baseURL="https://api-hmugo-web.itheima.net/api/public/v1";
+
+
+
+function First(props){
+  console.log('first组件。。。。。');
+  // eslint-disable-next-line react-hooks/rules-of-hooks
+  const [firstInfo, setFirstInfo] = useState([]);
+  const [carouselInfo, setCarouselInfo] = useState([]);
+  const [classifyNavInfo, setClassifyNavInfo] = useState([]);
+  const [floorInfo, setFloorInfo] = useState([])
+
+  // 获取轮播图数据
+  const getCarouselData = () => {
+    axios.get('/home/swiperdata').then(res => {
+      setCarouselInfo(res?.data?.message || [])
+    })
+  }
+  // 获取分类导航
+  const getClassifyNavData = () => {
+    axios.get('/home/catitems').then(res => {
+      setClassifyNavInfo(res?.data?.message || [])
+    })
+  }
+
+  // 获取楼层数据
+  const getFloorInfo = () => {
+    axios.get('/home/floordata').then(res => {    
+      setFloorInfo(res?.data?.message || [])
+    })
+  }
+
   useEffect(() => {
-    console.log(title);
-    console.log(props);
-  },[title])
-  return(
-    <div>
-      {title}
-      <Button onClick={() => settitle(title + 'e')}>Click</Button>
-    </div>
-  )
-}
+    getCarouselData();
+    getClassifyNavData();
+    getFloorInfo();
+  },[])
 
-function NumTest(props){
-  return <><span>{props.numContent}</span></>
-}
-
-function NumNone(props){
-  return <><span>{props.numContent}</span></>
-}
-function NumChange(props){
-  const {numContent, numClor} = props.dataSource;
-  if(numContent == '1000'){
-    return <NumTest numContent={numContent} />
-  }else{
-    return <NumNone numContent={numContent} />
-  }
-}
-function first(){
-  let dataSource = {
-    numContent: '1000',
-    numColor: 'successColor'
-  }
-
-  let testA = methostest1(5,7);
-  console.log(testA);
+  console.log(floorInfo);
   
   return(
     <div className='first-com'>
-     <h1>First-Component</h1>
-     <p>{testA}</p>
-      <Acom title='helloReactsss...'></Acom>
-      <NumChange dataSource={dataSource}></NumChange>
+      {/* 轮播图> */}
+      <Carousel>
+        {
+          carouselInfo && carouselInfo.map((item, index) => {
+            return(
+              <div key={index} className='every-carousel'>
+                <img src={item?.image_src} alt='' />
+              </div>
+            )
+          })
+        }
+      </Carousel>
+      {/* 分类导航 */}
+      <Row>
+        {
+          classifyNavInfo && classifyNavInfo.map((item, index) => {
+            return(
+              <Col key={index}>
+                <div>
+                  <img src={item.image_src} alt=""/>
+                </div>
+              </Col>
+            )
+          })
+        }
+      </Row>
+
+
+      {/* 楼层基本信息 */}
+      {
+        floorInfo && floorInfo.map((item, index) => {
+          return(
+            <div className='every-floor-info' key={index}>
+                <p>{item?.floor_title?.name}</p>
+                {
+                  item?.product_list && item?.product_list.map((_item, _index) => {
+                    return(
+                      <img key={_index} src={_item?.image_src} alt=""/>
+                    )
+                  })
+                }
+            </div>
+          )
+        })
+      }
     </div>
   )
 }
-export default withRouter(first);
+export default First;
 
 
